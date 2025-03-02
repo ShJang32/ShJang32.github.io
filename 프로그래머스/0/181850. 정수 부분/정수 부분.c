817607cc5dc58dc3a28d745f3e76f24889fa60b3
Seohee Jang <jangseohie32@gmail.com>
2025-03-02 10:20:29 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(double flo) {
+    int answer = 0;
+    // 실수 % 1 -> 정수만 출력
+    answer = flo / 1;
+    return answer;
+}
