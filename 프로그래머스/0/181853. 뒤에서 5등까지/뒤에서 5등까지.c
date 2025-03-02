0a7c7bf1c9fd2ed9ccde0e224fa01c336507a540
Seohee Jang <jangseohie32@gmail.com>
2025-03-02 10:52:19 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.c
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.c
@@ -5,7 +5,7 @@
 // num_list_len은 배열 num_list의 길이입니다.
 int* solution(int num_list[], size_t num_list_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(20);
+    int* answer = (int*)malloc(sizeof(int) * 5);
     int mininmum = 100;
     int temp = 0;
     
@@ -23,7 +23,9 @@ int* solution(int num_list[], size_t num_list_len) {
             }
         }
         if (flag == 0) {
-            answer = num_list;
+            for (int i=0; i<5; ++i) {
+                answer[i] = num_list[i];
+            }
             break;
         }
     }
